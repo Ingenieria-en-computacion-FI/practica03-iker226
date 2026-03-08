317600ee8b8ba227599450acc0a3d55eccaf8afd
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-03-07 22:38:12 -0600

    Update main.c

--- fraccion/main.c
+++ fraccion/main.c
@@ -1,43 +1,31 @@
-#include <stdio.h>
 #include "fraccion.h"
+#include <stdio.h>
 
-int main()
-{
-
+int main(){
     printf("Prueba del TAD Fraccion\n");
 
-    /* TODO
-       1 Crear dos fracciones
-       ejemplo: 1/2 y 3/4
-    */
-    Fraccion* f1 = NULL;
-    Fraccion* f2 = NULL;
+    Fraccion* f1=NULL;
+    Fraccion* f2=NULL;
 
+    f1=crearFraccion(1,2);
+    f2=crearFraccion(3,4);
 
     printf("Fraccion 1: ");
-    /* TODO imprimir */
-
+    imprimir(f1);
 
     printf("Fraccion 2: ");
-    /* TODO imprimir */
-
+    imprimir(f2);
 
     printf("\nSuma de fracciones:\n");
 
-    /* TODO
-       2 Sumar fracciones
-    */
-    Fraccion* resultado = NULL;
+    Fraccion* resultado=NULL;
+    resultado=sumar(f1, f2);
 
+    imprimir(resultado);
 
-    /* TODO
-       3 Imprimir resultado
-    */
-
-
-    /* TODO
-       4 Liberar memoria
-    */
+    destruir(f1);
+    destruir(f2);
+    destruir(resultado);
 
     return 0;
 }
